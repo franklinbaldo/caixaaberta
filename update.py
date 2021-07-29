6eed5f2d10a80adc17b8fb720c7e67f68b0203a6
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2021-07-28 22:04:38 -0400

    update

--- update.py
+++ update.py
@@ -31,16 +31,23 @@ def update_imoveis(file_path="imoveis_BR.csv"):
     new_df["fist_time_seen"] = None
     new_df = new_df.combine_first(old_df.loc[old_df.index.isin(new_df.index)])
     new_df["fist_time_seen"] = new_df["fist_time_seen"].fillna(value=new_df["data"])
-    
+
     removed_df = old_df.loc[~(old_df.index.isin(new_df.index))]
 
-    concated_df = pd.concat([new_df,removed_df] )
-    concated_df = concated_df.sort_index()
+    concated_df = (
+        pd.concat([new_df, removed_df]).sort_index().sort_values(["estado", "cidade"])
+    )
+    concated_df['data'] = pd.to_datetime(concated_df['data']).dt.date
+    concated_df['first_time_seen'] = pd.to_datetime(concated_df['first_time_seen']).dt.date
+
+    concated_df.to_csv(file_path)
+
     if file_path is not None:
         concated_df.to_csv(file_path)
         print("Arquivo atualizado com sucesso!", file_path)
 
     return concated_df
+
 
 if __name__ == "__main__":
     update_imoveis()
